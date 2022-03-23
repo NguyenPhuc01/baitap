0cd3016c794fdcae0b75807471c419ad32567c3d
nguyenvanphuc <phucnguyendx9012@gmail.com>
2022-03-23 21:03:18 +0700

    them custom forEach,Map

--- index.js
+++ index.js
@@ -22,9 +22,43 @@ function viec2(congViec) {
 }
 function main() {
     viec1(function () {
+        console.log('viec 1');
         viec2(function () {
             console.log('DA XONG CONG VIEC!!!!');
         });
     });
 }
 main()
+
+// Map
+Array.prototype.mymap = function (callback) {
+    const resultArray = [];
+    for (let index = 0; index < this.length; index++) {
+        resultArray.push(callback(this[index], index, this));
+    }
+    return resultArray;
+
+}
+const mang = [1, 2, 3]
+var output = mang.mymap(function (val, index, array) {
+    console.log(val, index, array);
+
+})
+
+
+
+
+//forEach
+Array.prototype.myForEach = function (callback) {
+    for (let i = 0; i < this.length; i++) {
+        callback(this[i]);
+    }
+};
+
+function callback(element) {
+    console.log(element); //insert logic
+}
+
+var array = [2, 4, 6, 8, 10];
+array.myForEach(callback);
+
